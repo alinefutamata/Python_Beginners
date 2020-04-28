d87b952641b842c33c456d8b59ae16331855f1d2
Aline Futamata <aline.f.lorenzon@gmail.com>
2020-04-28 22:59:27 +1200

    Day_9

---
+++ 2020.04.28 - Practice_1_ Dictionaries.py
@@ -0,0 +1,11 @@
+# Write a Python script to add a key to a dictionary.
+# Sample Dictionary : {0: 10, 1: 20}
+# Expected Result : {0: 10, 1: 20, 2: 30}
+
+dictionary = {
+    0: 10,
+    1: 20
+    }
+
+dictionary.update({2: 30})
+print(dictionary)
---
+++ 2020.04.28 - Practice_3_ Dictionaries.py
@@ -0,0 +1,8 @@
+# Write a Python script to check whether a given key already exists in a dictionary.
+
+professional = {
+    "name": "Aline Futamata",
+    "age": 28,
+    "employment_status": "Employed"
+}
+
