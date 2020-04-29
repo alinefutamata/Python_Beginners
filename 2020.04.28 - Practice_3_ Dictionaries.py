7eded977bbd0f16b57d00cfd55fa2b96eaaaea65
Aline Futamata <aline.f.lorenzon@gmail.com>
2020-04-29 20:55:08 +1200

    Day 10

--- 2020.04.28 - Practice_3_ Dictionaries.py
+++ 2020.04.28 - Practice_3_ Dictionaries.py
@@ -1,8 +1,2 @@
 # Write a Python script to check whether a given key already exists in a dictionary.
 
-professional = {
-    "name": "Aline Futamata",
-    "age": 28,
-    "employment_status": "Employed"
-}
-
